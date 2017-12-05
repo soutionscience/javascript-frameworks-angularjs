a6beca51829a25e69230b0be5b900e6bb6437e46
Rubin <marshnjagi@yahoo.com>
2017-12-05 22:07:07 +0300

    angular router basics

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -13,9 +13,17 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component'
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component'
+
+
+import { DishService } from './services/dish.service';
+
+
+import { AppRoutingModule } from './app-routing/app-routing.module'
 
 @NgModule({
   declarations: [
@@ -23,13 +31,17 @@ import { FooterComponent } from './footer/footer.component'
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    AppRoutingModule
   ],
   providers: [ DishService],
   bootstrap: [AppComponent]
